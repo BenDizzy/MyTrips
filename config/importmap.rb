f5013c46e487f86dd46a11794c695542a1d66b3e
ben_daniels <bendaniels00@gmail.com>
2025-04-17 22:00:28 -0400

    chore: pin mapbox-gl via CDN

---
+++ config/importmap.rb
@@ -0,0 +1,5 @@
+# Pin npm packages by running ./bin/importmap
+
+pin "application"
+pin "mapbox-gl", to: "https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm", preload: true
+pin "mapbox-gl", to: "https://cdn.jsdelivr.net/npm/bendaniels00@gmail.com/+esm", preload: true
